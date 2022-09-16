6fbf11ca3c6ea0554a0e1583527eef15fbf0cd38
zzc <1097954257@qq.com>
2022-09-16 17:26:01 +0800

    project init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Edit1
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
